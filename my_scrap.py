f50983974df8a5dc2db128b4d097034192602496
victorsouzw <svictorl@hotmail.com>
2024-06-20 19:19:55 -0300

    estrutura codigo em funcoes

--- my_scrap.py
+++ my_scrap.py
@@ -4,10 +4,11 @@ import json
 import pandas as pd
 from datetime import date
 
-client = requests.Session()
+
 
 HOMEPAGE_URL = 'https://www.linkedin.com'
 LOGIN_URL = 'https://www.linkedin.com/checkpoint/lg/login-submit'
+client = requests.Session()
 
 def salvar_detalhes_vaga(detalhes_vaga : json, url_vaga):
     titulo = detalhes_vaga['title']
@@ -24,7 +25,7 @@ def salvar_detalhes_vaga(detalhes_vaga : json, url_vaga):
     try:
         df_existente = pd.read_excel(excel_path)
         df_novos = pd.DataFrame(nova_vaga)
-        df_final = pd.concat([df_existente, df_novos], ignore_index=True)
+        df_final = pd.concat([df_existente, df_novos], ignore_index=True).drop_duplicates(subset='url_de_aplicacao', keep='first')
         
         with pd.ExcelWriter(excel_path, engine='xlsxwriter') as writer:
             df_final.to_excel(writer, index=False)
@@ -53,31 +54,22 @@ def extrair_detalhes_vaga(vaga_items):
         except Exception:
             pass        
     return None
+def login(email, senha):
+    global client
+    html = client.get(HOMEPAGE_URL).content
+    soup = BeautifulSoup(html, "html.parser")
+    csrf = soup.find('input', {"name":"loginCsrfParam"})['value']
 
-html = client.get(HOMEPAGE_URL).content
-soup = BeautifulSoup(html, "html.parser")
-csrf = soup.find('input', {"name":"loginCsrfParam"})['value']
+    login_information = {
+        'session_key': email, ###ALTERAR EMAIL
+        'session_password': senha,
+        'loginCsrfParam': csrf,
+    }
 
-file = open('senha.txt', 'r')
-login_information = {
-    'session_key':'starr6438@uorak.com', ###ALTERAR EMAIL
-    'session_password': file.read(),
-    'loginCsrfParam': csrf,
-}
-file.close()
-
-result_login = client.post(LOGIN_URL, data=login_information)
-result_login_soup = BeautifulSoup(result_login.content, "html.parser")
-if result_login_soup.find(id='captchaInternalPath') is not None:
-    raise Exception("O LinkedIn considerou o login suspeito. Solicitando um confirmação por código que foi enviado ao respectivo email. A resposta ao pin challenge não foi implementada.")
-
-# TODO deixar melhor a forma de adicionar a query
-keyword = input("Digite o a sua busca vaga como buscaria no LinkedIn: ")
-numero_paginas = int(input("Digite o numero de paginas que deseja buscar: "))
-url_vagas_inicio = 'https://www.linkedin.com/search/results/all/?'
-url_vagas_keywords = 'keywords='+keyword
-url_vagas_final = '&origin=GLOBAL_SEARCH_HEADER&sid=JTP'
-
+    result_login = client.post(LOGIN_URL, data=login_information)
+    result_login_soup = BeautifulSoup(result_login.content, "html.parser")
+    if result_login_soup.find(id='captchaInternalPath') is not None:
+        raise Exception("O LinkedIn considerou o login suspeito. Solicitando um confirmação por código que foi enviado ao respectivo email. A resposta ao pin challenge não foi implementada.")
 
 
 def extract_urls(data, key):
@@ -91,28 +83,46 @@ def extract_urls(data, key):
         for item in data:
             yield from extract_urls(item, key)
 
-https_urls = list()
 
-for i in range(0,numero_paginas):
-    content = client.get(
-        url_vagas_inicio + url_vagas_keywords + url_vagas_final + '&refres=true&start=' + str(25 * i)).content
-    soup = BeautifulSoup(content, "html.parser")
-    bpr_guid_tags = soup.select('code[id^="bpr-guid"]')
-    data = json.loads(str(bpr_guid_tags[-1].text).strip())
 
-    https_urls_dentro = (list(extract_urls(data, 'url')))
-    for url in https_urls_dentro:
-        if url not in https_urls:
-            https_urls.append(url)
+def gerar_urls(numero_paginas, url_busca):
+    global client
+    https_urls = list()
+    for i in range(0,numero_paginas):
+        content = client.get(
+            url_busca + '&refres=true&start=' + str(25 * i)).content
+        soup = BeautifulSoup(content, "html.parser")
+        bpr_guid_tags = soup.select('code[id^="bpr-guid"]')
+        data = json.loads(str(bpr_guid_tags[-1].text).strip())
 
-for url in https_urls:
-    # Send a GET request to the URL
-    response = client.get(url)
+        https_urls_dentro = (list(extract_urls(data, 'url')))
+        for url in https_urls_dentro:
+            if url not in https_urls:
+                https_urls.append(url)
+    return https_urls
 
-    # Parse the response content with BeautifulSoup
-    soup = BeautifulSoup(response.content, "html.parser")
+def buscar_e_salvar_vagas(numero_paginas, url_busca):
+    global client
+    https_urls = gerar_urls(numero_paginas, url_busca)
+    for url in https_urls:
+        # Send a GET request to the URL
+        response = client.get(url)
 
-    vaga_items = soup.find_all('code')
-    detalhes_vaga = extrair_detalhes_vaga(vaga_items)
-    if detalhes_vaga != None:
-        salvar_detalhes_vaga(detalhes_vaga, url)
+        # Parse the response content with BeautifulSoup
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        vaga_items = soup.find_all('code')
+        detalhes_vaga = extrair_detalhes_vaga(vaga_items)
+        if detalhes_vaga != None:
+            salvar_detalhes_vaga(detalhes_vaga, url)
+
+def main():
+    keyword = input("Digite o a sua busca vaga como buscaria no LinkedIn: ")
+    numero_paginas = int(input("Digite o numero de paginas que deseja buscar: "))
+    email = input("Digite o seu email: ")
+    senha = input("Digite o sua senha: ")
+    url_busca = 'https://www.linkedin.com/search/results/all/?keywords=' + keyword + '&origin=GLOBAL_SEARCH_HEADER&sid=JTP'
+    login(email, senha)
+    buscar_e_salvar_vagas(numero_paginas, url_busca)
+
+main()
